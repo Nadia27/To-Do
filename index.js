a8e4c898627b78d4b0a265ceba8c6f58cd1b314d
Nadia27 <jones.nadia.l@gmail.com>
2019-01-26 20:28:09 -0500

    add basic functions of text field and and adding new items to list

---
+++ index.js
@@ -0,0 +1,19 @@
+// Add task to list when enter/add item button is pressed
+// Remove text from field when enter/add item button is pressed
+// Remove task from list when completed
+
+
+var inputField = document.getElementById("input-field");
+
+inputField.addEventListener("keyup", function(event) {
+	event.preventDefault();
+	if (event.keyCode === 13) { // keyCode === 13 enter key
+		document.querySelector("button").click(); //
+    var listItem = document.createElement("LI"); // create li element
+		var userInput = document.getElementById("input-field").value; // grab text from field
+    var textNode = document.createTextNode(userInput); // create text node from user input
+    listItem.appendChild(textNode); // append li element to text node
+    document.getElementById("list").appendChild(listItem); // append new list item to ul
+    inputField.value = ''; // clear text field after submit
+	}
+});
