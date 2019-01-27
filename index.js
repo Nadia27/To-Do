d41f627b0b81d0b39ad8bb017c622f018bb296c8
Nadia27 <jones.nadia.l@gmail.com>
2019-01-27 15:27:59 -0500

    added click listener to task list

--- index.js
+++ index.js
@@ -4,16 +4,30 @@
 
 
 var inputField = document.getElementById("input-field");
+var listItem = document.getElementsByClassName("todo-item");
 
 inputField.addEventListener("keyup", function(event) {
-	event.preventDefault();
-	if (event.keyCode === 13) { // keyCode === 13 enter key
-		document.querySelector("button").click(); // simulate mouse click if "enter" button is pressed
-    var listItem = document.createElement("LI"); // create li element
-		var userInput = document.getElementById("input-field").value; // grab text from field
+  event.preventDefault();
+  if (event.keyCode === 13) { // keyCode === 13 enter key
+    document.querySelector("button").click(); // simulate mouse click if "enter" button is pressed
+    var li = document.createElement("LI"); // create li element
+    li.classList.add("todo-item");
+    var userInput = document.getElementById("input-field").value; // grab text from field
     var textNode = document.createTextNode(userInput); // create text node from user input
-    listItem.appendChild(textNode); // append li element to text node
-    document.getElementById("list").appendChild(listItem); // append new list item to ul
+    li.appendChild(textNode); // append li element to text node
+    document.getElementById("list").appendChild(li); // append new list item to ul
     inputField.value = ''; // clear text field after submit
-	}
+    if (listItem.length > 0) {
+      for (var i = 0; i < listItem.length; i++) {
+        listItem[i].classList.add("-" + i);
+        listItem[i].addEventListener("click", deleteTask);
+      }
+    }
+    console.log(listItem);
+  }
 });
+
+// add EventListener to listItems
+function deleteTask () {
+  alert("I was clicked");
+}
