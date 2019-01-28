9db89817bbed75436988219b90c68cf2dc483ebd
Nadia27 <jones.nadia.l@gmail.com>
2019-01-28 13:30:36 -0500

    fixed submit button and refactor function for adding task to list

--- index.js
+++ index.js
@@ -2,32 +2,44 @@
 // Remove text from field when enter/add item button is pressed
 // Remove task from list when completed
 
-
 var inputField = document.getElementById("input-field");
 var listItem = document.getElementsByClassName("todo-item");
+var button = document.getElementById("submit-btn");
 
-inputField.addEventListener("keyup", function(event) {
-  event.preventDefault();
+
+inputField.addEventListener("keyup", function() {
   if (event.keyCode === 13) { // keyCode === 13 enter key
-    document.querySelector("button").click(); // simulate mouse click if "enter" button is pressed
-    var li = document.createElement("LI"); // create li element
-    li.classList.add("todo-item");
-    var userInput = document.getElementById("input-field").value; // grab text from field
-    var textNode = document.createTextNode(userInput); // create text node from user input
-    li.appendChild(textNode); // append li element to text node
-    document.getElementById("list").appendChild(li); // append new list item to ul
-    inputField.value = ''; // clear text field after submit
-    if (listItem.length > 0) {
-      for (var i = 0; i < listItem.length; i++) {
-        listItem[i].classList.add("-" + i);
-        listItem[i].addEventListener("click", deleteTask);
-      }
-    }
-    console.log(listItem);
+    event.preventDefault();
+    inputTask();
   }
 });
 
+button.addEventListener("click", inputTask);
+
+function inputTask(event) {
+  var li = document.createElement("LI"); // create li element
+  li.classList.add("todo-item");
+  var userInput = document.getElementById("input-field").value; // grab text from field
+  var textNode = document.createTextNode(userInput); // create text node from user input
+  li.appendChild(textNode); // append li element to text node
+  document.getElementById("list").appendChild(li); // append new list item to ul
+  inputField.value = ''; // clear text field after submit
+  if (listItem.length > 0) { // check if list exist
+    for (i = 0; i < listItem.length; i++) {
+      listItem[i].setAttribute("id", i); // add unique id to each li
+      listItem[i].addEventListener("click", deleteTask); // add click listener
+    }
+  }
+  console.log(listItem);
+
+}
+
+
+
 // add EventListener to listItems
-function deleteTask () {
-  alert("I was clicked");
+function deleteTask() {
+  var item = document.getElementById(this.id);
+  item.classList.add("completed");
+  console.log(item);
+
 }
